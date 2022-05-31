e199c43edae5cc8212357101cb8e4a6154241855
AyeItsAxi <tristangee1@gmail.com>
2022-05-30 23:19:09 -0700

    Immense trolling has commenced.

---
+++ RiftSkinUnlockerRewrite/PatchService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace RiftSkinUnlockerRewrite
+{
+    internal class PatchService
+    {
+        public static void Init()
+        {
+            Console.WriteLine("Locating Rift (Version 2.2.0.5-B)");
+            System.Threading.Thread.Sleep(759);
+            Console.WriteLine("Located Rift!\n\nInjecting code into Harmony");
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Injecting code into Harmony.");
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Injecting code into Harmony..");
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Injecting code into Harmony...");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("Code successfully injected into Harmony!\n\nSaving assembly");
+            System.Threading.Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("Saving assembly.");
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Saving assembly..");
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Saving assembly...");
+            System.Threading.Thread.Sleep(3629);
+            Console.Clear();
+            Console.WriteLine("rift skins have been added to the fortnite");
+            System.Diagnostics.Process.Start(@"C:\Program Files\Internet Explorer\iexplore.exe", "http://www.youtube.com/watch?v=dQw4w9WgXcQ");
+            Console.ReadLine();
+            for(; ; )
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+        }
+    }
+}
---
+++ RiftSkinUnlockerRewrite/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RiftSkinUnlockerRewrite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Rift skin unlocker!\nThis was made by ozymandias to unlock skins in Rift!\n\nPress any key to continue...");
+            Console.ReadKey(true);
+            PatchService.Init();
+        }
+    }
+}
